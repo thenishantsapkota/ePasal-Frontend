7cbb4b2d3caaef08c52d9ca6fbf4c127797d295f
Nishant Sapkota <snishant306@gmail.com>
2024-06-06 11:51:21 +0545

    chore: fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { CartProvider } from './contexts/CartContext.jsx'
+import App from './App'
+import { CartProvider } from './contexts/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
