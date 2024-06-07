18e6623a81be1f1d962ac4140095e995bd89e0c3
Nishant Sapkota <snishant306@gmail.com>
2024-06-07 07:19:19 +0545

    chore: add automatic token inference using axios interceptors

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,14 +4,17 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-api.interceptors.request.use((config) =>{ 
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if(token){
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-})
+);
